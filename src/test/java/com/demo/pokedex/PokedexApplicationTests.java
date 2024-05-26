f5e9da8d6dd66c57569194b7b38bc43b7e9fb8fc
Cristian Olivares <85cristianolivares@gmail.com>
2024-05-25 22:58:05 -0400

    chore: Add initial project files and configuration

---
+++ src/test/java/com/demo/pokedex/PokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
