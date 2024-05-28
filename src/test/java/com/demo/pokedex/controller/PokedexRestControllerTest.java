2ec0412d1151af23df6f9ad3fc9dcc649a3302ef
Cristian Olivares <85cristianolivares@gmail.com>
2024-05-28 00:25:01 -0400

    chore: Add PokedexRestControllerTest for testing getPokemon endpoint

---
+++ src/test/java/com/demo/pokedex/controller/PokedexRestControllerTest.java
@@ -0,0 +1,22 @@
+package com.demo.pokedex.controller;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import com.demo.pokedex.service.PokemonService;
+
+public class PokedexRestControllerTest {
+
+    @BeforeEach
+    public void setup() {
+        pokemonService = mock(PokemonService.class);
+        
+    }
+
+    @Test
+    public void testGetPokemon() {
+        fail();
+    }
+}
